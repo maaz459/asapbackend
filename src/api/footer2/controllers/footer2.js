8633df1d19a2a68dec3c9c5644869782bdd91f88
maaz459 <maazhussain097@gmail.com>
2022-02-04 14:11:56 +0000

    new schema added

--- src/api/footer2/controllers/footer2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  footer2 controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::footer2.footer2');
--- src/api/footer2/routes/footer2.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * footer2 router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::footer2.footer2');
